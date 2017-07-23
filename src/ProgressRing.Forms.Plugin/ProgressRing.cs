22a4bfcff2ca8c9bd6a99492ce66612152ab48f7
Andreas Hennig <hello@hennigandreas.de>
2017-07-23 13:44:24 +0200

    Fixed issue where the AnimatedProgress property couldn't be set in XAML

--- src/ProgressRing.Forms.Plugin/ProgressRing.cs
+++ src/ProgressRing.Forms.Plugin/ProgressRing.cs
@@ -2,39 +2,62 @@
 
 namespace ProgressRingControl.Forms.Plugin
 {
-    public class ProgressRing : ProgressBar
+    // Version 0.2: 
+	// TODO: Add validation method for progress values
+	// TODO: Add Animation easing property (for binding) + use in AnimatedProgress
+	// TODO: Add animation time property (for binding) + use in AnimatedProgress
+	// TODO: Add circle fill
+
+	public class ProgressRing : ProgressBar
     {
-		// Source: https://www.jimbobbennett.io/animating-xamarin-forms-progress-bars/
-		public static readonly BindableProperty AnimatedProgressProperty = BindableProperty.Create("AnimatedProgress", typeof(double),
-                                                                                                   typeof(ProgressRing), 0.5);
+        #region Properties
+
+        // Source: https://www.jimbobbennett.io/animating-xamarin-forms-progress-bars/
+        public static readonly BindableProperty AnimatedProgressProperty = BindableProperty.Create("AnimatedProgress", typeof(double),
+                                                                                                   typeof(ProgressRing), 0.5,
+                                                                                                   propertyChanged: HandleAnimatedProgressBindablePropertyChanged);
         public double AnimatedProgress
         {
             get { return (double)base.GetValue(AnimatedProgressProperty); }
-            set {
-				ViewExtensions.CancelAnimations(this);
-				ProgressTo(value, 800, Easing.SinOut);
+            set
+            {
+                base.SetValue(AnimatedProgressProperty, value);
+
+                ViewExtensions.CancelAnimations(this);
+                ProgressTo(value, 800, Easing.SinOut);
             }
         }
 
         public static readonly BindableProperty RingProgressColorProperty = BindableProperty.Create("RingProgressColor", typeof(Color),
-                                                                                                   typeof(ProgressRing), Color.FromRgb(234, 105, 92));
-        public Color RingProgressColor {
+                                                                                                    typeof(ProgressRing), Color.FromRgb(234, 105, 92));
+        public Color RingProgressColor
+        {
             get { return (Color)base.GetValue(RingProgressColorProperty); }
             set { base.SetValue(RingProgressColorProperty, value); }
         }
 
-        public static readonly BindableProperty RingBaseColorProperty = BindableProperty.Create("RingBaseColor", typeof(Color), 
-                                                                                            typeof(ProgressRing), Color.FromRgb(46, 60, 76));
-        public Color RingBaseColor {
-            get { return (Color)base.GetValue(RingBaseColorProperty); } 
+        public static readonly BindableProperty RingBaseColorProperty = BindableProperty.Create("RingBaseColor", typeof(Color),
+                                                                                                typeof(ProgressRing), Color.FromRgb(46, 60, 76));
+        public Color RingBaseColor
+        {
+            get { return (Color)base.GetValue(RingBaseColorProperty); }
             set { base.SetValue(RingBaseColorProperty, value); }
         }
 
-		public static readonly BindableProperty RingThicknessProperty = BindableProperty.Create("RingThickness", typeof(double),
+        public static readonly BindableProperty RingThicknessProperty = BindableProperty.Create("RingThickness", typeof(double),
                                                                                                 typeof(ProgressRing), 5.0);
-        public double RingThickness {
+        public double RingThickness
+        {
             get { return (double)base.GetValue(RingThicknessProperty); }
             set { base.SetValue(RingThicknessProperty, value); }
         }
-    }
+
+		#endregion
+
+		static void HandleAnimatedProgressBindablePropertyChanged(BindableObject bindable, object oldValue, object newValue)
+		{
+            var progress = (ProgressRing)bindable;
+		    progress.AnimatedProgress = (double)newValue;
+		}
+	}
 }
