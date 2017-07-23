bb4e3720949dd487e9b1be6ecc0715205e7ce324
Andreas Hennig <hello@hennigandreas.de>
2017-07-23 14:08:03 +0200

    added comments

--- src/ProgressRing.Forms.Plugin/ProgressRing.cs
+++ src/ProgressRing.Forms.Plugin/ProgressRing.cs
@@ -3,16 +3,20 @@
 namespace ProgressRingControl.Forms.Plugin
 {
     // Version 0.2: 
-	// TODO: Add validation method for progress values
-	// DONE: Add Animation easing property (for binding) + use in AnimatedProgress
-	// DONE: Add animation time property (for binding) + use in AnimatedProgress
-	// TODO: Add circle fill
+    // TODO: AnimatedProgress / Progress: Test if value coerce is necessary
+    // DONE: Add Animation easing property (for binding) + use in AnimatedProgress
+    // DONE: Add animation time property (for binding) + use in AnimatedProgress
+    // TODO: Add circle fill
 
-	public class ProgressRing : ProgressBar
+    public class ProgressRing : ProgressBar
     {
         #region Properties
 
         // Source: https://www.jimbobbennett.io/animating-xamarin-forms-progress-bars/
+        /// <summary>
+        /// Let's you animate from the current progress to a new progress using
+        /// the values of the properties AnimationLength and AnimationEasing.
+        /// </summary>
         public static readonly BindableProperty AnimatedProgressProperty = BindableProperty.Create("AnimatedProgress", typeof(double),
                                                                                                    typeof(ProgressRing), 0.5,
                                                                                                    propertyChanged: HandleAnimatedProgressBindablePropertyChanged);
@@ -28,6 +32,10 @@ namespace ProgressRingControl.Forms.Plugin
             }
         }
 
+        /// <summary>
+        /// Set's the animation length that is used when using the AnimatedProgress
+        /// property to animate to a certain progress.
+        /// </summary>
         public static readonly BindableProperty AnimationLengthProperty = BindableProperty.Create("AnimationLength", typeof(uint),
                                                                                                   typeof(ProgressRing), 800);
         public uint AnimationLength {
@@ -35,6 +43,10 @@ namespace ProgressRingControl.Forms.Plugin
             set { base.SetValue(AnimationLengthProperty, value); } 
         }
 
+        /// <summary>
+        /// Set's the easing function that is used when using the AnimatedProgress
+        /// property to animate to a certain progress.
+        /// </summary>
         public static readonly BindableProperty AnimationEasingProperty = BindableProperty.Create("AnimationEasing", typeof(Easing),
                                                                                                   typeof(ProgressRing), Easing.Linear);
 
@@ -43,6 +55,10 @@ namespace ProgressRingControl.Forms.Plugin
             set { base.SetValue(AnimationEasingProperty, value); }
         }
 
+        /// <summary>
+        /// Sets the ring's progress color. 
+        /// HINT: The ring progress color covers the ring base color
+        /// </summary>
         public static readonly BindableProperty RingProgressColorProperty = BindableProperty.Create("RingProgressColor", typeof(Color),
                                                                                                     typeof(ProgressRing), Color.FromRgb(234, 105, 92));
         public Color RingProgressColor
@@ -51,6 +67,9 @@ namespace ProgressRingControl.Forms.Plugin
             set { base.SetValue(RingProgressColorProperty, value); }
         }
 
+        /// <summary>
+        /// Sets the ring's base (background) color.
+        /// </summary>
         public static readonly BindableProperty RingBaseColorProperty = BindableProperty.Create("RingBaseColor", typeof(Color),
                                                                                                 typeof(ProgressRing), Color.FromRgb(46, 60, 76));
         public Color RingBaseColor
@@ -59,6 +78,11 @@ namespace ProgressRingControl.Forms.Plugin
             set { base.SetValue(RingBaseColorProperty, value); }
         }
 
+        /// <summary>
+        /// Sets the thickness of the progress Ring. The thickness "grows" into the
+        /// center of the ring (so the outer dimensions are not incluenced by the
+        /// ring thickess.
+        /// </summary>
         public static readonly BindableProperty RingThicknessProperty = BindableProperty.Create("RingThickness", typeof(double),
                                                                                                 typeof(ProgressRing), 5.0);
         public double RingThickness
@@ -67,12 +91,12 @@ namespace ProgressRingControl.Forms.Plugin
             set { base.SetValue(RingThicknessProperty, value); }
         }
 
-		#endregion
+        #endregion
 
-		static void HandleAnimatedProgressBindablePropertyChanged(BindableObject bindable, object oldValue, object newValue)
-		{
+        static void HandleAnimatedProgressBindablePropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
             var progress = (ProgressRing)bindable;
-		    progress.AnimatedProgress = (double)newValue;
-		}
-	}
+            progress.AnimatedProgress = (double)newValue;
+        }
+    }
 }
