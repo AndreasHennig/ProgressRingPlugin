851f6394279d81c72efd202c7243485d2710cc22
Ali Yousefi <ali.visual.studio@gmail.com>
2018-03-18 18:43:50 +0330

    fix bug

--- src/ProgressRing.Forms.Plugin.Android/ProgressRingRenderer.cs
+++ src/ProgressRing.Forms.Plugin.Android/ProgressRingRenderer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Android.Graphics;
+using Android.OS;
 using ProgressRingControl.Forms.Plugin;
 using ProgressRingControl.Forms.Plugin.Android;
 using Xamarin.Forms;
@@ -11,7 +12,7 @@ namespace ProgressRingControl.Forms.Plugin.Android
 {
     public class ProgressRingRenderer : ViewRenderer
     {
-        private  Paint _paint;
+        private Paint _paint;
         private RectF _ringDrawArea;
         private bool _sizeChanged = false;
 
@@ -20,10 +21,14 @@ namespace ProgressRingControl.Forms.Plugin.Android
             SetWillNotDraw(false);
         }
 
+        bool IsApi19()
+        {
+            return ((int)Build.VERSION.SdkInt) == 19;
+        }
+
         protected override void OnDraw(Canvas canvas)
         {
             var progressRing = (ProgressRing)Element;
-
             if (_paint == null)
             {
                 var displayDensity = Context.Resources.DisplayMetrics.Density;
@@ -35,28 +40,33 @@ namespace ProgressRingControl.Forms.Plugin.Android
                 _paint.Flags = PaintFlags.AntiAlias;
             }
 
-            if(_ringDrawArea == null || _sizeChanged) {
+            if (_ringDrawArea == null || _sizeChanged || IsApi19())
+            {
                 _sizeChanged = false;
 
                 var ringAreaSize = Math.Min(canvas.ClipBounds.Width(), canvas.ClipBounds.Height());
-               
-                var ringDiameter = ringAreaSize - _paint.StrokeWidth;
 
-                var left = canvas.ClipBounds.CenterX() - ringDiameter / 2;
-                var top = canvas.ClipBounds.CenterY() - ringDiameter / 2;
+                var ringDiameter = Math.Abs(ringAreaSize - _paint.StrokeWidth);
+                var centerX = canvas.ClipBounds.CenterX();
+                var centerY = canvas.ClipBounds.CenterY();
+                var left = centerX - ringDiameter / 2;
+                var top = centerY - ringDiameter / 2;
 
                 _ringDrawArea = new RectF(left, top, left + ringDiameter, top + ringDiameter);
+                if (IsApi19())
+                    canvas.ClipRect(new RectF(0, 0, canvas.Width, canvas.Height), Region.Op.Replace);
             }
 
-			var backColor = progressRing.RingBaseColor;
-			var frontColor = progressRing.RingProgressColor;
-			var progress = (float)progressRing.Progress;
+            var backColor = progressRing.RingBaseColor;
+            var frontColor = progressRing.RingProgressColor;
+            var progress = (float)progressRing.Progress;
             DrawProgressRing(canvas, progress, backColor, frontColor);
         }
 
         private void DrawProgressRing(Canvas canvas, float progress,
                                       Color ringBaseColor,
-                                      Color ringProgressColor) {
+                                      Color ringProgressColor)
+        {
             _paint.Color = ringBaseColor.ToAndroid();
             canvas.DrawArc(_ringDrawArea, 270, 360, false, _paint);
 
